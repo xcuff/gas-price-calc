c62f7f69e8847bfef3aa92a88e882e495520b6d5
XCUFF <situatedgull28@gmail.com>
2022-06-08 18:38:52 -0500

    i broke everything

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
+
+int doMath(int distance, int consumption, int fuel, int costpergal, int totalfuelcost)
+{
+    int o = distance / consumption;
+    totalfuelcost = o * costpergal;
+
+    return totalfuelcost;
+}
+
 int main ()
 {
+    int distance;
+    int consumption;
+    int fuel;
+    int costpergal;
+    int totalfuelcost;
 
+    printf("Lets calculate some gas. \n\nFirst off, make sure you have some stuff, \n\n\ncost per gal, \ndistance & fuel efficiency(consumption per mile)\n");
+
+    printf("\n\n Give me your cost Per Gal: ");
+    scanf("costpergal %d", &costpergal);
+    printf("\n\n Give me your distance: ");
+    scanf("distance %d", &distance);
+    printf("\n\n Give me your consumption rate: ");
+    scanf("consumption %d", &consumption);
+    int output = doMath(distance, consumption, fuel, costpergal, totalfuelcost);
+
+    return 0;
 }
